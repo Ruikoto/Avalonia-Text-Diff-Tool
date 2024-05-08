afbbf2f0fb932a2628d54ddfcca3bf42665316a9
Zhang Zhenyu <30781137+Ruikoto@users.noreply.github.com>
2024-05-08 14:10:18 +0800

    chore: update HighlightBackgroundRenderer to support highlighting specific ranges in addition to lines

--- TextDiff_Demo/Utils/HighlightBackgroundRenderer.cs
+++ TextDiff_Demo/Utils/HighlightBackgroundRenderer.cs
@@ -1,33 +1,56 @@
-﻿using System.Linq;
-using Avalonia.Media;
+﻿using Avalonia.Media;
 using AvaloniaEdit;
 using AvaloniaEdit.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace TextDiff_Demo.Utils;
-
-public class HighlightBackgroundRenderer : IBackgroundRenderer
+namespace TextDiff_Demo.Utils
 {
-    public KnownLayer Layer => KnownLayer.Background;
-
-    private TextEditor _editor;
-    private readonly int _lineNumber;
-    private readonly IBrush _lineBrush;
-
-    public HighlightBackgroundRenderer(TextEditor editor, int lineNumber, IBrush lineBrush)
+    public class HighlightBackgroundRenderer : IBackgroundRenderer
     {
-        _editor = editor;
-        _lineNumber = lineNumber;
-        _lineBrush = lineBrush;
-    }
+        public KnownLayer Layer => KnownLayer.Background;
 
-    public void Draw(TextView textView, DrawingContext drawingContext)
-    {
-        if (!textView.VisualLinesValid) return;
-        foreach (var line in textView.VisualLines)
+        private readonly TextEditor _editor;
+        private readonly List<int> _linesToHighlight;
+        private readonly Dictionary<int, List<(int Start, int Length)>> _lineRangesToHighlight;
+        private readonly IBrush _lineBrush;
+        private readonly IBrush _rangeBrush;
+
+        public HighlightBackgroundRenderer(TextEditor editor, IEnumerable<int> linesToHighlight, Dictionary<int, List<(int Start, int Length)>> lineRangesToHighlight, IBrush lineBrush, IBrush rangeBrush)
         {
-            if (line.FirstDocumentLine.LineNumber != _lineNumber) continue;
-            var rect = BackgroundGeometryBuilder.GetRectsFromVisualSegment(textView, line, 0, line.VisualLength).First();
-            drawingContext.DrawRectangle(_lineBrush, null, rect);
+            _editor = editor;
+            _linesToHighlight = linesToHighlight.ToList();
+            _lineRangesToHighlight = lineRangesToHighlight;
+            _lineBrush = lineBrush;
+            _rangeBrush = rangeBrush;
+        }
+
+        public void Draw(TextView textView, DrawingContext drawingContext)
+        {
+            if (!textView.VisualLinesValid) return;
+
+            foreach (var line in textView.VisualLines)
+            {
+                // 高亮整行背景
+                if (_linesToHighlight.Contains(line.FirstDocumentLine.LineNumber))
+                {
+                    var rect = BackgroundGeometryBuilder.GetRectsFromVisualSegment(textView, line, 0, line.VisualLength).First();
+                    drawingContext.DrawRectangle(_lineBrush, null, rect);
+                }
+
+                // 高亮行中的特定范围
+                if (_lineRangesToHighlight.TryGetValue(line.FirstDocumentLine.LineNumber, out var ranges))
+                {
+                    foreach (var (start, length) in ranges)
+                    {
+                        var rects = BackgroundGeometryBuilder.GetRectsFromVisualSegment(textView, line, start, length);
+                        foreach (var rect in rects)
+                        {
+                            drawingContext.DrawRectangle(_rangeBrush, null, rect);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- TextDiff_Demo/Views/DiffView.axaml.cs
+++ TextDiff_Demo/Views/DiffView.axaml.cs
@@ -1,7 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
 using Avalonia.Media;
+using AvaloniaEdit;
 using TextDiff_Demo.Utils;
-using TextDiff_Demo.ViewModels;
 
 namespace TextDiff_Demo.Views
 {
@@ -9,22 +10,27 @@ namespace TextDiff_Demo.Views
     {
         public DiffView()
         {
-            DiffViewModel vm = new();
-            DataContext = vm;
             InitializeComponent();
             AddHighlightRenderer();
         }
 
         private void AddHighlightRenderer()
         {
-            // 设定要高亮的行号
-            int lineNumber = 3;
+            // 设置需要高亮的行号和行内的范围
+            var linesToHighlight = new[] { 2, 3, 5 };
+            var lineRangesToHighlight = new Dictionary<int, List<(int Start, int Length)>>
+            {
+                { 2, [(3, 5), (15, 4)] }, // 第2行，从索引3到5，以及索引15到4个字符
+                { 3, [(10, 7)] },
+                { 5, [(8, 10)] }
+            };
 
-            // 创建背景颜色刷
+            // 定义背景颜色刷
             IBrush lineBrush = new SolidColorBrush(Color.Parse("#FFDDDDDD"));
+            IBrush rangeBrush = new SolidColorBrush(Color.Parse("#FFFFAACC"));
 
             // 创建并添加渲染器
-            var highlightRenderer = new HighlightBackgroundRenderer(textEditor, lineNumber, lineBrush);
+            var highlightRenderer = new HighlightBackgroundRenderer(textEditor, linesToHighlight, lineRangesToHighlight, lineBrush, rangeBrush);
             textEditor.TextArea.TextView.BackgroundRenderers.Add(highlightRenderer);
         }
     }
